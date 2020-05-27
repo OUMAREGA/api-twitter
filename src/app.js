0ef1b6cfd8ba89763fda326d63ba6e08b8c05f3b
Willy <w.douangpaseut@ecole-ipssi.net>
2020-05-27 11:43:35 +0200

    fix app.js and format usersController

--- src/app.js
+++ src/app.js
@@ -18,29 +18,12 @@ app.use(session({
     store: new MongoStore({ mongooseConnection: mongoose.connection })
 }));
 
-const bodyParser = require("body-parser");
-
-const userRoutes = require('./routes/routesUser');
-var session = require('express-session')
-
-app.use(session({
-    secret: 'P)j5yBV(kShrY{*@',
-    resave: false,
-    saveUninitialized: false,
-  }))
-
-mongoose.connect("mongodb://mongo/api_twitter_BDD");
-
 app.set("view engine", "ejs"); //règle pour associer le moteur de templating de express à ejs
 app.set("views", "views"); //éviter de préciser le chemin de la vue, directement préciser le nom du fichier
 
 app.use('/bs', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(userRoutes());
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(userRoutes());
